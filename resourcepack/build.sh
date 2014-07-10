d48ca81bc7184af5233bc621c6b5e94822003eb6
theoriginalbit <theoriginalbit@me.com>
2014-07-10 21:00:11 +1000

    okay resource pack build script actually works nicely now

--- resourcepack/build.sh
+++ resourcepack/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+echo ================ THEORIGINALBIT RESOURCE PACK BUILDER ================
 echo Which resourcepack would you like to zip? 
 
 read file
@@ -10,7 +12,13 @@ zipFile=$a$file$b
 
 if [ -e $file ]; then
 	if [ -d $file ]; then
-		zip -r "$zipFile" $file -x "*.DS_Store"
+		echo ============================ ZIPPING PACK ============================
+		cd $file
+		zip -r "$zipFile" . -x "*.DS_Store"
+		cd ..
+		mv "$file/$zipFile" .
+		echo ============================== COMPLETE ==============================
+		echo Pack created as \'$zipFile\'
 	else
 		echo Not a folder
 	fi
