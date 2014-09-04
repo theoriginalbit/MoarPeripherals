5a2690eb7cfd438f31bced86f462d86a42d60f32
theoriginalbit <theoriginalbit@me.com>
2014-09-04 13:15:59 +1000

    Created project for MC 1.7.x

---
+++ README.md
@@ -0,0 +1,14 @@
+<p align="center">
+  <img src="http://i.imgur.com/IY49GPE.png" alt="MoarPeripherals"/>
+</p>
+
+A Minecraft mod that adds more peripherals into the ComputerCraft mod
+
+<a href="http://www.computercraft.info/forums2/index.php?/topic/19357-" target="_blank">Official Thread</a>
+
+<a href="http://wiki.theoriginalbit.com/moarperipherals/" target="_blank">Official Wiki</a>
+
+License
+-------
+
+MoarPeripherals is distributed under the terms of the MIT License, which can be found under the name LICENSE in your distribution.
---
+++ src/main/java/com/theoriginalbit/moarperipherals/MoarPeripherals.java
@@ -0,0 +1,62 @@
+package com.theoriginalbit.moarperipherals;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStoppingEvent;
+
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Joshua Asbury (@theoriginalbit)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+@SuppressWarnings("unused")
+@Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION, dependencies = ModInfo.DEPENDENCIES)
+public class MoarPeripherals {
+
+    @Mod.Instance(ModInfo.ID)
+    public static MoarPeripherals instance;
+
+    public static boolean isServerStopping = false;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    @SuppressWarnings("unused")
+    public void serverStopping(FMLServerStoppingEvent e) {
+        isServerStopping = true;
+    }
+
+}
