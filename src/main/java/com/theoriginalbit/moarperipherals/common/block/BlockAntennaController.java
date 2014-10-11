444f24d0f0472e576386dd7b9ee618b61cc72d92
theoriginalbit <theoriginalbit@me.com>
2014-10-11 21:20:17 +1100

    removed redundant code

--- src/main/java/com/theoriginalbit/moarperipherals/common/block/BlockAntennaController.java
+++ src/main/java/com/theoriginalbit/moarperipherals/common/block/BlockAntennaController.java
@@ -15,9 +15,7 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.ForgeDirection;
 
 public class BlockAntennaController extends BlockAntenna {
 
@@ -40,11 +38,6 @@ public class BlockAntennaController extends BlockAntenna {
     @Override
     public TileEntity createNewTileEntity(World world, int par1) {
         return new TileAntennaController();
-    }
-
-    @Override
-    public boolean isBlockSolid(IBlockAccess blockAccess, int x, int y, int z, int side) {
-        return ForgeDirection.values()[side] == ForgeDirection.DOWN;
     }
 
 }
