b2b1f136d07bff76ccc70deced3b6f136011d4d7
theoriginalbit <theoriginalbit@me.com>
2014-10-12 23:43:26 +1100

    improved ChatBox range set validation

--- src/main/java/com/theoriginalbit/moarperipherals/common/tile/TileChatBox.java
+++ src/main/java/com/theoriginalbit/moarperipherals/common/tile/TileChatBox.java
@@ -42,7 +42,8 @@ import java.util.List;
 public class TileChatBox extends TileMoarP implements IBreakAwareTile, IChatListener, IDeathListener, ICommandListener {
 
     private static final String ERROR_TOO_MANY = "too many messages (max %d per second)";
-    private static final String ERROR_RANGE = "range must be between -1 and the server default";
+    private static final String ERROR_RANGE_FORMAT = "range must be between -1 (infinite) and %d inclusive";
+    private static final String ERROR_RANGE_GENERIC = "range must be -1 (infinite) or higher";
     private static final String ERROR_LABEL = "label must be no more than %d characters long";
 
     private static final String EVENT_CHAT = "chat_message";
@@ -109,11 +110,12 @@ public class TileChatBox extends TileMoarP implements IBreakAwareTile, IChatList
 
     @LuaFunction(isMultiReturn = true)
     public Object[] setSayRange(int range) {
-        if (range < -1 || (ConfigHandler.chatRangeSay > -1 && range > ConfigHandler.chatRangeSay)) {
-            return new Object[]{false, ERROR_RANGE};
+        final Object[] valid = validateRange(range, ConfigHandler.chatRangeSay);
+        // if the new range was accepted
+        if ((Boolean) valid[0]) {
+            rangeSay = range;
         }
-        rangeSay = range;
-        return new Object[]{true};
+        return valid;
     }
 
     @LuaFunction
@@ -128,11 +130,12 @@ public class TileChatBox extends TileMoarP implements IBreakAwareTile, IChatList
 
     @LuaFunction(isMultiReturn = true)
     public Object[] setTellRange(int range) {
-        if (range < -1 || (ConfigHandler.chatRangeTell > -1 && range > ConfigHandler.chatRangeTell)) {
-            return new Object[]{false, ERROR_RANGE};
+        final Object[] valid = validateRange(range, ConfigHandler.chatRangeTell);
+        // if the new range was accepted
+        if ((Boolean) valid[0]) {
+            rangeTell = range;
         }
-        rangeTell = range;
-        return new Object[]{true};
+        return valid;
     }
 
     @LuaFunction
@@ -147,11 +150,12 @@ public class TileChatBox extends TileMoarP implements IBreakAwareTile, IChatList
 
     @LuaFunction(isMultiReturn = true)
     public Object[] setReadRange(int range) {
-        if (range < -1 || (ConfigHandler.chatRangeRead > -1 && range > ConfigHandler.chatRangeRead)) {
-            return new Object[]{false, ERROR_RANGE};
+        final Object[] valid = validateRange(range, ConfigHandler.chatRangeRead);
+        // if the new range was accepted
+        if ((Boolean) valid[0]) {
+            rangeRead = range;
         }
-        rangeRead = range;
-        return new Object[]{true};
+        return valid;
     }
 
     @LuaFunction
@@ -254,6 +258,25 @@ public class TileChatBox extends TileMoarP implements IBreakAwareTile, IChatList
             ChatBoxHandler.instance.removeDeathListener(this);
             ChatBoxHandler.instance.removeCommandListener(this);
         }
+    }
+
+    private Object[] validateRange(int range, int server) {
+        // if they want a value below -1, which is a NO-OP number
+        if (range < -1) {
+            // if the server range is -1, they can set range to whatever they want, so tell them that otherwise
+            // tell them the maximum they can use (which in reality they should check themselves too!)
+            return new Object[]{false, server != -1 ? String.format(ERROR_RANGE_FORMAT, server) : ERROR_RANGE_GENERIC};
+        }
+        // if the server has a max specified that isn't infinite, and they want infinite, reject it
+        if (range == -1 && server != -1) {
+            return new Object[]{false, String.format(ERROR_RANGE_FORMAT, server)};
+        }
+        // if the server has a max specified that isn't infinite, and they want a higher value, reject it
+        if (range > server && server != -1) {
+            return new Object[]{false, String.format(ERROR_RANGE_FORMAT, server)};
+        }
+        // the number they want is valid
+        return new Object[]{true};
     }
 
     private boolean entityInRange(Entity entity, int range) {
