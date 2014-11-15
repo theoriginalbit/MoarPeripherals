7d7a8903b6326ff39b9391b781143f371d1e7530
theoriginalbit <theoriginalbit@me.com>
2014-11-15 12:09:40 +1100

    FIX: Attempting to fix mod asset extraction issue on Windows

--- src/main/java/com/theoriginalbit/moarperipherals/common/reference/ModInfo.java
+++ src/main/java/com/theoriginalbit/moarperipherals/common/reference/ModInfo.java
@@ -23,7 +23,7 @@ public final class ModInfo {
     public static final String CHANNEL = ID.toLowerCase();
     public static final String RESOURCE_DOMAIN = ID.toLowerCase();
     public static final String CONFIG_DOMAIN = RESOURCE_DOMAIN + ".config.";
-    public static final String LUA_PATH = "assets/" + RESOURCE_DOMAIN + "/lua";
+    public static final String LUA_PATH = "/assets/" + RESOURCE_DOMAIN + "/lua";
     public static final String EXTRACTED_LUA_PATH = "mods/" + RESOURCE_DOMAIN + "/lua";
 
     private static final String COM_PACKAGE = "com.theoriginalbit";
